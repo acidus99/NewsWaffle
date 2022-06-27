95161d15a9edc3e4861ed7b76c895c3977400eb6
Acidus <acidus99@users.noreply.github.com>
2022-06-26 21:18:40 -0400

    Adding in Console app

---
+++ NewsWaffle.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using NewsWaffle;
+
+namespace NewsWaffle.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CgiRouter router = new CgiRouter();
+            router.OnRequest("", RouteHandler.Welcome);
+            router.ProcessRequest();
+        }
+    }
+}
