06e63fa28a431d5e2df0dae095179f8e67e19783
Acidus <acidus99@users.noreply.github.com>
2022-06-26 23:53:23 -0400

    adding more test sites

--- NewsWaffle.Cgi/RouteHandler.cs
+++ NewsWaffle.Cgi/RouteHandler.cs
@@ -84,8 +84,9 @@ namespace NewsWaffle.Cgi
         {
             cgi.Success();
             cgi.Writer.WriteLine("# 🧇 NewsWaffle");
-            cgi.Writer.WriteLine("=> /cgi-bin/waffle.cgi/portal?https%3A%2F%2Fwww.wired.com%2F Wired.com");
-            cgi.Writer.WriteLine("=> /cgi-bin/waffle.cgi/portal?https%3A%2F%2Fwww.irishtimes.com%2F Irish Times");
+            cgi.Writer.WriteLine("=> /cgi-bin/waffle.cgi/portal?https%3A%2F%2Fwww.wired.com%2F Wired");
+            cgi.Writer.WriteLine("=> /cgi-bin/waffle.cgi/portal?https%3A%2F%2Fwww.ajc.com%2F Atlanta Journal-Constitution");
+            cgi.Writer.WriteLine("=> /cgi-bin/waffle.cgi/portal?https%3A%2F%2Fwww.sixcolors.com%2F Six Colors");
             Footer(cgi);
         }
 
