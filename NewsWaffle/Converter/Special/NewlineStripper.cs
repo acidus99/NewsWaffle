7f8b4057872256d4a04883555301ac9d42fb029e
Acidus <acidus99@users.noreply.github.com>
2022-06-27 18:32:08 -0400

    centralize text stripping

---
+++ NewsWaffle/Converter/Special/NewlineStripper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace NewsWaffle.Converter.Special
+{
+	public static class NewlineStripper
+	{
+		private static readonly Regex whitespace = new Regex(@"\s+", RegexOptions.Compiled);
+
+        public static string RemoveNewlines(string text)
+        {
+            text = text.Replace("\r", " ");
+            text = text.Replace("\n", " ");
+            text = whitespace.Replace(text, " ");
+            return text;
+        }
+    }
+}
+
