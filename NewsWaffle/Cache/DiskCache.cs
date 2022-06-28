bcffb54a503a529149e2977496ddb906db145420
Acidus <acidus99@users.noreply.github.com>
2022-06-27 20:37:34 -0400

    use DiskCache from Gemipedia

---
+++ NewsWaffle/Cache/DiskCache.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.IO;
+
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NewsWaffle.Cache
+{
+	public class DiskCache
+	{
+		static readonly TimeSpan DefaultLifetime = TimeSpan.FromHours(2);
+
+		private TimeSpan Lifespan;
+
+		public DiskCache()
+		{
+			Lifespan = DefaultLifetime;
+		}
+
+		public DiskCache(TimeSpan lifetime)
+			: this()
+        {
+			Lifespan = lifetime;
+        }
+
+		public void Clear(string identifier)
+        {
+			//get the key
+			var cacheKey = GetCacheKey(identifier);
+			var filepath = GetPath(cacheKey);
+			try
+			{
+				File.Delete(filepath);
+			}
+			catch (Exception)
+			{
+
+			}
+		}
+
+		public string GetAsString(string identifier)
+        {
+
+			//get the key
+			var cacheKey = GetCacheKey(identifier);
+			var filepath = GetPath(cacheKey);
+			try
+			{
+				if (CacheEntryValid(filepath))
+				{
+					return File.ReadAllText(filepath);
+				}
+			}
+			catch(Exception)
+            {
+
+            }
+			return null;
+        }
+
+		public byte [] GetAsBytes(string identifier)
+		{
+
+			//get the key
+			var cacheKey = GetCacheKey(identifier);
+			var filepath = GetPath(cacheKey);
+			try
+			{
+				if (CacheEntryValid(filepath))
+				{
+					return File.ReadAllBytes(filepath);
+				}
+			}
+			catch (Exception)
+			{
+
+			}
+			return null;
+		}
+
+		public bool Set(string identifier, string contents)
+        {
+			var cacheKey = GetCacheKey(identifier);
+			var filepath = GetPath(cacheKey);
+			try
+			{
+				File.WriteAllText(filepath, contents);
+				return true;
+			}
+			catch (Exception)
+			{
+
+			}
+			return false;
+		}
+
+		public bool Set(string identifier, byte [] bytes)
+		{
+			var cacheKey = GetCacheKey(identifier);
+			var filepath = GetPath(cacheKey);
+			try
+			{
+				File.WriteAllBytes(filepath, bytes);
+				return true;
+			}
+			catch (Exception)
+			{
+
+			}
+			return false;
+		}
+
+		private string GetPath(string cacheKey)
+			=> Path.Combine(Path.GetTempPath(), cacheKey + ".diskcache");
+
+		private bool CacheEntryValid(string filepath)
+        {
+			try
+			{
+				return (File.GetLastWriteTime(filepath) >= DateTime.Now.Subtract(Lifespan));
+			}
+			catch (Exception)
+			{ }
+			return false;
+		}
+
+		private string GetCacheKey(string identifier)
+			=> Convert.ToHexString(MD5.HashData(Encoding.UTF8.GetBytes(identifier)));
+	}
+}
+
--- NewsWaffle/Net/HttpFetch.cs
+++ NewsWaffle/Net/HttpFetch.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Net;
+
+using NewsWaffle.Cache;
+
 namespace NewsWaffle.Net
 {
     public class HttpFetcher
     {
-        WebClient client;
+        WebClient Client;
+        DiskCache Cache;
+
 
         public HttpFetcher()
         {
-            client = new WebClient();
-            client.Headers.Add(HttpRequestHeader.UserAgent, "GeminiProxy/0.1 (gemini://gemi.dev/; acidus@gemi.dev) gemini-proxy/0.1");
+            Client = new WebClient();
+            Client.Headers.Add(HttpRequestHeader.UserAgent, "GeminiProxy/0.1 (gemini://gemi.dev/; acidus@gemi.dev) gemini-proxy/0.1");
+            Cache = new DiskCache(TimeSpan.FromHours(1));
         }
 
         public string GetHtml(string url)
-            => client.DownloadString(url);
+        {
+            //first check the cache
+            var contents = Cache.GetAsString(url);
+            if (contents != null)
+            {
+                return contents;
+            }
+            //fetch it
+            contents = Client.DownloadString(url);
+            //cache it
+            Cache.Set(url, contents);
+            return contents;
+        }
     }
 }
