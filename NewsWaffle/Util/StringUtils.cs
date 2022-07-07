6cf90a67f1765744df3ca8b3ea1901ab1b8ef321
Acidus <acidus99@users.noreply.github.com>
2022-07-07 17:33:19 -0400

    create common StringUtils

---
+++ NewsWaffle/Util/StringUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace NewsWaffle.Util
+{
+	public static class StringUtils
+	{
+        private static readonly Regex whitespace = new Regex(@"\s+", RegexOptions.Compiled);
+
+        /// <summary>
+        /// normalizes a string found in HTML
+        /// - HTML decodes it
+        /// - strips any remaining HTML tags
+        /// - converts \n, \t, and \r tabs to space
+        /// - collapses runs of whitespace into a single space
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static string Normnalize(string s)
+        {
+            //decode
+            s = WebUtility.HtmlDecode(s);
+            //strip tags
+            s = Regex.Replace(s, @"<[^>]*>", "");
+            if (s.Contains('\t'))
+            {
+                s.Replace('\t', ' ');
+            }
+            return RemoveNewlines(s);
+        }
+
+        public static string RemoveNewlines(string text)
+        {
+            if (text.Length > 0 && (text.Contains('\n') || text.Contains('\r')))
+            {
+                text = text.Replace('\r', ' ');
+                text = text.Replace('\n', ' ');
+                text = whitespace.Replace(text, " ");
+            }
+            return text;
+        }
+    }
+}
