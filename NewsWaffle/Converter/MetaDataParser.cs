bb0ca798d80875be35b729d014600a03c7262112
Acidus <acidus99@users.noreply.github.com>
2022-07-07 17:32:44 -0400

    new metadata parser

---
+++ NewsWaffle/Converter/MetaDataParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using OpenGraphNet;
+using NewsWaffle.Models;
+using NewsWaffle.Util;
+
+namespace NewsWaffle.Converter
+{
+	/// <summary>
+    /// Extracts meta data from the HTML
+    /// </summary>
+	public class MetaDataParser
+	{
+		OpenGraph openGraph;
+
+		public PageMetaData GetMetaData(string url, string html)
+		{
+			openGraph = OpenGraph.ParseHtml(html);
+
+			return new PageMetaData
+			{
+				Description = GetDescription(),
+				FeaturedImage = GetFeatureImage(),
+				OriginalUrl = url,
+				Title = GetTitle(),
+				Type = GetOpenGraphType()
+			};
+		}
+
+		private string GetDescription()
+			=> StringUtils.Normnalize(openGraph.Metadata["og:description"].FirstOrDefault()?.Value ?? "");
+
+		private string GetFeatureImage()
+			=> openGraph.Image?.AbsoluteUri ?? null;
+
+		private string GetTitle()
+			//TODO use HTML as well
+			=> StringUtils.Normnalize(openGraph.Title ?? "");
+
+		private string GetOpenGraphType()
+			=> openGraph.Type;
+
+	}
+}
+
---
+++ NewsWaffle/Models/PageMetaData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace NewsWaffle.Models
+{
+	public class PageMetaData
+	{
+		public string Description { get; internal set; }
+		public string FeaturedImage { get; internal set; }
+		public string OriginalUrl { get; internal set; }
+		public string Title { get; internal set; }
+		public string Type { get; internal set; }
+
+	}
+}
+
