a2a9d818f5e7fe35a8646748d500fdea2a49162f
Acidus <acidus99@users.noreply.github.com>
2022-07-08 18:55:32 -0400

    Simplify paths

--- NewsWaffle.Cgi/CgiPaths.cs
+++ NewsWaffle.Cgi/CgiPaths.cs
@@ -5,18 +5,19 @@ namespace NewsWaffle.Cgi
 	public static class CgiPaths
 	{
 		public const string BasePath = "/cgi-bin/waffle.cgi";
-		public const string ArticlePath = BasePath + "/article";
-		public const string FeedPath = BasePath + "/feed";
-		public const string ViewPath = BasePath + "/view";
-
-		public static string ViewAuto(string url)
-			=> $"{ViewPath}?{WebUtility.UrlEncode(url)}";
 
 		public static string ViewArticle(string url)
-			=> $"{ViewArticle}?{WebUtility.UrlEncode(url)}";
+			=> $"{BasePath}/article?{WebUtility.UrlEncode(url)}";
+
+		public static string ViewAuto(string url)
+			=> $"{BasePath}/view?{WebUtility.UrlEncode(url)}";
 
 		public static string ViewFeed(string feedUrl)
-			=> $"{FeedPath}?{WebUtility.UrlEncode(feedUrl)}";
+			=> $"{BasePath}/feed?{WebUtility.UrlEncode(feedUrl)}";
+
+		public static string ViewLinks(string url)
+			=> $"{BasePath}/links?{WebUtility.UrlEncode(url)}";
+
 	}
 }
 
