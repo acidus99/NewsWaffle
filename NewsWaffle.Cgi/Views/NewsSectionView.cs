494e1825a9fe61130decacd470bc772a8d376de7
Acidus <acidus99@users.noreply.github.com>
2022-12-18 13:35:46 -0500

    Break into 2 specific views

---
+++ NewsWaffle.Cgi/Views/NewsSectionView.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Gemini.Cgi;
+
+using NewsWaffle.Aggregators;
+using NewsWaffle.Aggregators.Models;
+using NewsWaffle.Models;
+
+namespace NewsWaffle.Cgi.Views
+{
+	internal class NewsSectionView : AbstractView
+    {
+        NewsSection NewsSection;
+
+        protected override IPageStats PageStats => NewsSection;
+
+        public NewsSectionView(StreamWriter sw, NewsSection newsSection)
+            : base(sw)
+        {
+            NewsSection = newsSection;
+        }
+
+        protected override void RenderView()
+        {
+            RenderTitle("Current News");
+            Out.WriteLine($"=> {CgiPaths.SwitchNewsSection} Current Section: {NewsSection.SectionName}. Change?");
+            Out.WriteLine();
+            if (NewsSection.Stories.Count > 0)
+            {
+                int counter = 0;
+                foreach (var story in NewsSection.Stories.OrderByDescending(x => x.Updated))
+                {
+                    counter++;
+                    Out.WriteLine($"=> {CgiPaths.ViewArticle(story.Url)} {counter}. {story.Title} ({story.Source}, {story.TimeAgo})");
+                }
+            }
+            else
+            {
+                Out.WriteLine("This sections doesn't have any news stories"); ;
+            }
+        }
+    }
+}
---
+++ NewsWaffle.Cgi/Views/SectionSelectView.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Gemini.Cgi;
+
+using NewsWaffle.Aggregators;
+using NewsWaffle.Aggregators.Models;
+using NewsWaffle.Models;
+
+namespace NewsWaffle.Cgi.Views
+{
+	internal class SectionSelectView : AbstractView
+    {
+        INewAggregator Aggregator;
+
+        public SectionSelectView(StreamWriter sw, INewAggregator aggregator)
+            : base(sw)
+        {
+            Aggregator = aggregator;
+        }
+
+        protected override IPageStats PageStats => null;
+
+        protected override void RenderView()
+        {
+            RenderTitle("Current News");
+            Out.WriteLine($"Aggregator: {Aggregator.Name}");
+            Out.WriteLine("Choose a section:");
+            foreach (var section in Aggregator.AvailableSections)
+            {
+                Out.WriteLine($"=> {CgiPaths.ViewCurrentNewsSection(section)} {section}");
+            }
+        }
+    }
+}
