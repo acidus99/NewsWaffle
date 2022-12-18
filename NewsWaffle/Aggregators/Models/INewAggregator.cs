c0bfbd1a836b98ad55f90daefceeafd8572e9750
Acidus <acidus99@users.noreply.github.com>
2022-12-18 12:51:23 -0500

    Update INewAggregator.cs

---
+++ NewsWaffle/Aggregators/Models/INewAggregator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace NewsWaffle.Aggregators.Models
+{ 
+	public interface INewAggregator
+	{
+		/// <summary>
+    /// The name of the news aggregator
+    /// </summary>
+		public string Name { get; }
+
+    public string DefaultSection { get; }
+
+    public string [] AvailableSections { get; }
+
+		/// <summary>
+    /// Returns the web resource to request for a section
+    /// </summary>
+    /// <param name="type"></param>
+    /// <returns></returns>
+		public string GetFeedUrl(string sectionName);
+
+		/// <summary>
+    /// parses the web resource for a section
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns></returns>
+		public NewsSection ParseSection(string sectionName, string content, int downloadTime);
+	}
+}
